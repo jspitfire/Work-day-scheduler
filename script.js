4fdff16765eb51ffdb3a40ce7518c4a4babc3e7c
James Fryer <james.fryer@live.co.uk>
2024-01-21 17:51:49 +0000

    pseudocode added for function required in script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+//Function to display current day at the top of the calendar
+
+
+//Function to generate time blocks for 9-5 busines hours
+
+
+//Color-code time blocks based on past, present and future
+
+
+
+//Function to handle click on save button and save event to local storage
+
+
+
+
+// Function to load events from local storage and populate time blocks
+
+
+
+
+
+// Call functions to initialize the planner
